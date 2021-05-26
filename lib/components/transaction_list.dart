ddfbd00f9cdf63946a3c8f08cebefd9638642faf
hugofsd <hugo619@outlook.com>
2021-05-26 00:35:02 -0300

    ListView

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -13,55 +13,53 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300, //tamanlho para scroll
-      child: SingleChildScrollView(
-        //scroll para rolagem
-        child: Column(
-          children: transactions.map((tr) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 10,
-                    ),
-                    decoration: BoxDecoration(
-                        border: Border.all(
+      child: ListView(
+        // lista para scroll q precisa de um pai com tamanho
+        children: transactions.map((tr) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  )),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${tr.value.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
                       color: Colors.purple,
-                      width: 2,
-                    )),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'R\$ ${tr.value.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        color: Colors.purple,
-                      ),
                     ),
                   ),
-                  Column(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold, //peso da fonte
-                          ),
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold, //peso da fonte
                         ),
                       ),
-                      Text(
-                        DateFormat('d/M/y').format(tr.date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(), // to list para converter para uma lista
-        ),
+                    ),
+                    Text(
+                      DateFormat('d/M/y').format(tr.date),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        }).toList(), // to list para converter para uma lista
       ),
     );
   }
